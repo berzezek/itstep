e4c8f79323338cebeb1cae234b724598b5dc5c11
berzezek <wknduz@gmail.com>
2024-02-06 21:54:27 +0500

    lesson_02_CW

--- samplesite/bboard/models.py
+++ samplesite/bboard/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Рубрика"
+        verbose_name_plural = "Рубрики"
+        ordering = ["name"]
+
+
+class Bb(models.Model):
+    rubric = models.ForeignKey(Rubric, null=True, on_delete=models.PROTECT, verbose_name="Рубрика")
+    title = models.CharField(max_length=50)
+    content = models.TextField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+    published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+        ordering = ["-published"]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,5 +1,30 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Bb, Rubric
+from django.views.generic.edit import CreateView
+from .forms import BbForm
+from django.urls import reverse_lazy
+
+
+class BbCreateView(CreateView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 
 
 def index(request):
-    return HttpResponse("Hello world!")
+    bbs = Bb.objects.order_by("-published")
+    rubrics = Rubric.objects.all()
+    context = {"bbs": bbs, "rubrics": rubrics}
+    return render(request, "bboard/index.html", context)
+
+def by_rubric(index, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {"bbs": bbs, "rubrics": rubrics, "current_rubric": current_rubric}
+    return render(index, "bboard/by_rubric.html", context)
