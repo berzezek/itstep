88548816d2ef3e89736169486fd62299081ffcaf
berzezek <wknduz@gmail.com>
2024-02-07 17:08:14 +0500

    complete 31_CW

--- samplesite/bboard/middleware.py
+++ samplesite/bboard/middleware.py
@@ -1,13 +1,37 @@
-from .models import Bb
+from .models import Bb, Rubric
+
 
 def simple_middleware(next):
     def core_middleware(request):
+        print("before")
         response = next(request)
-        print(request.readlines())
+        print("after")
         return response
+
     return core_middleware
+
+
+class RubricsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
+
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        response = self.get_response(request)
+        # Code to be executed for each request/response after
+        # the view is called.
+
+        return response
+
+    def process_template_response(self, request, response):
+        response.context_data["rubrics"] = Rubric.objects.all()
+        return response
+
 
 from django.contrib.auth.models import User
 
-def all_users(request):
-    return {'all_users': User.objects.all()}
+
+def rubrics(request):
+    return {"rubrics": Rubric.objects.all()}
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.template.response import TemplateResponse
 from .models import Bb, Rubric
 from django.views.generic.edit import CreateView
 from .forms import BbForm
@@ -6,25 +7,28 @@ from django.urls import reverse_lazy
 
 
 class BbCreateView(CreateView):
-    template_name = 'bboard/create.html'
+    template_name = "bboard/create.html"
     form_class = BbForm
-    success_url = reverse_lazy('index')
-    
+    success_url = reverse_lazy("index")
+
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
-        context['rubrics'] = Rubric.objects.all()
+        context["rubrics"] = Rubric.objects.all()
         return context
 
 
 def index(request):
     bbs = Bb.objects.order_by("-published")
-    rubrics = Rubric.objects.all()
-    context = {"bbs": bbs, "rubrics": rubrics}
-    return render(request, "bboard/index.html", context)
+    # rubrics = Rubric.objects.all()
+    context = {"bbs": bbs}
+    # response = render(request, "bboard/index.html", context)
+    # print(type(response))
+    return TemplateResponse(request, "bboard/index.html", context)
 
-def by_rubric(index, rubric_id):
+
+def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
     rubrics = Rubric.objects.all()
     current_rubric = Rubric.objects.get(pk=rubric_id)
     context = {"bbs": bbs, "rubrics": rubrics, "current_rubric": current_rubric}
-    return render(index, "bboard/by_rubric.html", context)
+    return render(request, "bboard/by_rubric.html", context)
